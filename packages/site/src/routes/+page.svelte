00741372b7ae210138cea17be2be420bfe935c50
Daniel Sykes <sykespro@gmail.com>
2023-02-08 19:55:39 -0500

    initial commit

---
+++ packages/site/src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<h1>Svelte Tampa Bay</h1>
+
+<div class=" m-8 ">
+    <form action="">
+        <input type="text" name="email">
+        <button class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-full shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Subscribe</button>
+    </form>
+</div>
---
+++ packages/site/tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{html,js,svelte,ts}', '../../packages/components/src/**/*.svelte'],
+	theme: {
+		extend: {}
+	},
+	plugins: [require('@tailwindcss/forms')]
+};
